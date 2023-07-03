d6f0a1458d523eebc23d056de3c534f264f831d5
justin connors <justinconnors@hotmail.ca>
2023-07-03 15:06:24 -0300

    queries added for user and metrics in client dir

---
+++ client/src/utils/queries.js
@@ -0,0 +1,49 @@
+import { gql } from "@apollo/client";
+
+// User query to get user data
+export const QUERY_USER = gql`
+  query user {
+    user {
+      _id
+      firstName
+      lastName
+      email
+    }
+  }
+`;
+
+// User query to get user data by ID
+export const QUERY_USER_BY_ID = gql`
+  query userById($userId: ID!) {
+    userById(userId: $userId) {
+      _id
+      firstName
+      lastName
+      email
+    }
+  }
+`;
+
+// Query to get all metrics
+export const QUERY_METRICS = gql`
+  query metrics {
+    metrics {
+      _id
+      name
+      labels
+      values
+    }
+  }
+`;
+
+// Query to get a single metric
+export const QUERY_METRIC = gql`
+  query metric($id: ID!) {
+    metric(id: $id) {
+      _id
+      name
+      labels
+      values
+    }
+  }
+`;
